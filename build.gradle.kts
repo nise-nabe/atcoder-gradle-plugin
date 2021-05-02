5414ef0e02ee418f8b4382b19ff3a5c555e9b62a
nise_nabe <nise.nabe@gmail.com>
2021-05-02 19:09:23 +0900

    add http client dependencies and test

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,20 @@ group = "com.nisecoder.gradle"
 version = "1.0-SNAPSHOT"
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
+
+    implementation("it.skrape:skrapeit-core:1.1.1")
+    implementation("io.github.rybalkinsd:kohttp:0.12.0")
+
+
+    testImplementation(gradleTestKit())
+    testImplementation(kotlin("test-junit5"))
+    testImplementation(platform("org.junit:junit-bom:5.7.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
 
 
