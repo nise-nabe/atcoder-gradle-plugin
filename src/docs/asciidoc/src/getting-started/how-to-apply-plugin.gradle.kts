b4eedbcdcf454e9e2d01abdc3dc96f2573c957a2
nise_nabe <nise.nabe@gmail.com>
2021-05-04 13:14:34 +0900

    fix how to apply plugin

---
+++ src/docs/asciidoc/src/getting-started/how-to-apply-plugin.gradle.kts
@@ -0,0 +1,20 @@
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/nise-nabe/atcoder-gradle-plugin")
+            mavenContent {
+                includeGroup("com.nisecoder.gradle")
+            }
+        }
+        gradlePluginPortal()
+
+    }
+
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.startsWith("com.nisecoder.gradle.atcoder")) {
+                useModule("com.nisecoder.gradle:atcoder-gradle-plugin:1.0-SNAPSHOT")
+            }
+        }
+    }
+}
