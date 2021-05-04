630500ce3a1f966634ec706ffbce63d27d99cd26
nise_nabe <nise.nabe@gmail.com>
2021-05-04 22:26:51 +0900

    delete task list from root

--- src/main/kotlin/com/nisecoder/gradle/atcoder.gradle.kts
+++ src/main/kotlin/com/nisecoder/gradle/atcoder.gradle.kts
@@ -2,7 +2,6 @@ package com.nisecoder.gradle
 
 import com.nisecoder.gradle.atcoder.task.AtCoderLoginTask
 import com.nisecoder.gradle.atcoder.task.AtCoderNewContestTask
-import com.nisecoder.gradle.atcoder.task.AtCoderTaskListTask
 
 plugins {
     id("com.nisecoder.gradle.atcoder.credentials")
@@ -14,9 +13,5 @@ tasks {
     register<AtCoderNewContestTask>("atcoderNew") {
         sessionFile.set(atcoderLogin.sessionFile)
         outputDir.set(project.rootDir.resolve("subprojects/contests/"))
-    }
-
-    register<AtCoderTaskListTask>("atcoderTaskList") {
-        sessionFile.set(atcoderLogin.sessionFile)
     }
 }
