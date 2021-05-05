9715ef3c83b279ac48d40507f9feaffc86bb0420
nise_nabe <nise.nabe@gmail.com>
2021-05-05 16:05:28 +0900

    fix document to use github package

--- src/docs/sample/getting-started/settings.gradle.kts
+++ src/docs/sample/getting-started/settings.gradle.kts
@@ -1,13 +1,18 @@
 pluginManagement {
     repositories {
-        maven {
-            url = uri("https://maven.pkg.github.com/nise-nabe/atcoder-gradle-plugin")
-            mavenContent {
+        gradlePluginPortal()
+        exclusiveContent {
+            forRepository {
+                maven {
+                    name = "GithubPackagesAtCoderGradlePlugin"
+                    url = uri("https://maven.pkg.github.com/nise-nabe/atcoder-gradle-plugin")
+                    credentials(PasswordCredentials::class)
+                }
+            }
+            filter {
                 includeGroup("com.nisecoder.gradle")
             }
         }
-        gradlePluginPortal()
-
     }
 
     resolutionStrategy {
@@ -18,5 +23,10 @@ pluginManagement {
         }
     }
 }
+
+plugins {
+    id("com.nisecoder.gradle.atcoder.auto-include")
+}
+
 
 rootProject.name = "getting-started"
