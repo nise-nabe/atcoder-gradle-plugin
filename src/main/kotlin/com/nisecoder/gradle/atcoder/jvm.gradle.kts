6dcbc822c9d0f985a13d04c4c6387e2c25665490
nise_nabe <nise.nabe@gmail.com>
2021-05-06 11:41:54 +0900

    add test sourceSet feature

--- src/main/kotlin/com/nisecoder/gradle/atcoder/jvm.gradle.kts
+++ src/main/kotlin/com/nisecoder/gradle/atcoder/jvm.gradle.kts
@@ -12,9 +12,10 @@ java {
 }
 
 val atcoder = extensions.getByType<AtCoderExtension>()
+val javaPluginExtension = extensions.getByType<JavaPluginExtension>()
 
 atcoder.contestTask.all {
-    sourceSets.create(name) {
+    val mainSourceSet = sourceSets.create(name) {
         val mainSourceSet = sourceSets[SourceSet.MAIN_SOURCE_SET_NAME]
         val mainOutput = objects.fileCollection().from(mainSourceSet.output)
         compileClasspath += mainOutput
@@ -22,5 +23,28 @@ atcoder.contestTask.all {
 
         configurations.getByName(implementationConfigurationName)
             .extendsFrom(configurations.getByName(mainSourceSet.implementationConfigurationName))
+    }
+
+    val testSourceSet = sourceSets.create("${name}Test") {
+        val testSourceSet = sourceSets[SourceSet.TEST_SOURCE_SET_NAME]
+        val testOutput = objects.fileCollection().from(testSourceSet.output)
+        compileClasspath += mainSourceSet.output + testOutput
+        runtimeClasspath += mainSourceSet.output + testOutput
+
+        configurations.getByName(implementationConfigurationName)
+            .extendsFrom(configurations.getByName(testSourceSet.implementationConfigurationName))
+    }
+
+    val testTask = tasks.register<Test>("test$name") {
+        description = "Runs the unit tests."
+        group = JavaBasePlugin.VERIFICATION_GROUP
+
+        testClassesDirs = testSourceSet.output.classesDirs
+        classpath = testSourceSet.runtimeClasspath
+        modularity.inferModulePath.convention(javaPluginExtension.modularity.inferModulePath)
+    }
+
+    tasks.named(JavaBasePlugin.CHECK_TASK_NAME) {
+        dependsOn(testTask)
     }
 }
