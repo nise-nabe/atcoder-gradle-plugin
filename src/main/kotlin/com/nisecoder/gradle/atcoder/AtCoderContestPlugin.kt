25335ab5cc84f54b1d63079185e78b7b7585825f
nise_nabe <nise.nabe@gmail.com>
2021-08-14 22:43:22 +0900

    cleanup

--- src/main/kotlin/com/nisecoder/gradle/atcoder/AtCoderContestPlugin.kt
+++ src/main/kotlin/com/nisecoder/gradle/atcoder/AtCoderContestPlugin.kt
@@ -17,12 +17,10 @@ import org.gradle.jvm.toolchain.JavaCompiler
 import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.jvm.toolchain.JavaToolchainService
 import org.gradle.kotlin.dsl.create
-import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.kotlin.dsl.named
 import org.gradle.kotlin.dsl.register
 import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
 import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
