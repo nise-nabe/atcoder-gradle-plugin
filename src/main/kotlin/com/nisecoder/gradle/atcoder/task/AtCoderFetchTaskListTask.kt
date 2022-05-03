dcc805d1889317f3806726b5f7325cf10fcaf2d8
nise_nabe <nise.nabe@gmail.com>
2022-05-03 09:32:46 +0900

    add session task for build service injection

--- src/main/kotlin/com/nisecoder/gradle/atcoder/task/AtCoderFetchTaskListTask.kt
+++ src/main/kotlin/com/nisecoder/gradle/atcoder/task/AtCoderFetchTaskListTask.kt
@@ -1,29 +1,22 @@
 package com.nisecoder.gradle.atcoder.task
 
-import com.nisecoder.gradle.atcoder.AtCoderBuildService
 import com.nisecoder.gradle.atcoder.internal.AtCoderFetcher
-import com.nisecoder.gradle.atcoder.internal.readFirstLine
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.provider.Property
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.InputFile
-import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.OutputFile
 import org.gradle.api.tasks.TaskAction
 
-abstract class AtCoderFetchTaskListTask: AtCoderTask() {
+abstract class AtCoderFetchTaskListTask: AtCoderSessionTask() {
     @get:Input
     abstract val contestName: Property<String>
 
     @get:OutputFile
     abstract val taskListFile: RegularFileProperty
 
-    @get:Internal
-    abstract val atCoderService: Property<AtCoderBuildService>
-
     @TaskAction
     fun taskList() {
-        val fetcher = AtCoderFetcher(atCoderService.get().login())
+        val fetcher = AtCoderFetcher(atcoderService.get().login())
 
         val result = fetcher.fetchTaskList(contestName.get())
         taskListFile.get().asFile.printWriter().use { writer ->
--- src/main/kotlin/com/nisecoder/gradle/atcoder/task/AtCoderLoginTask.kt
+++ src/main/kotlin/com/nisecoder/gradle/atcoder/task/AtCoderLoginTask.kt
@@ -1,14 +1,8 @@
 package com.nisecoder.gradle.atcoder.task
 
-import com.nisecoder.gradle.atcoder.AtCoderBuildService
-import org.gradle.api.provider.Property
-import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.TaskAction
 
-abstract class AtCoderLoginTask : AtCoderTask() {
-    @get:Internal
-    abstract val atcoderService: Property<AtCoderBuildService>
-
+abstract class AtCoderLoginTask : AtCoderSessionTask() {
     @TaskAction
     fun login() {
         atcoderService.get().run {
